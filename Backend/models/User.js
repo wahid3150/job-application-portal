6b3a8005ab916b9772b3879e9ecc05fb18b341da
Wahid ali <wahidali3150@gmail.com>
2026-01-15 13:05:02 +0500

    Password encryption/decryption are remvoed from model will be implemented in controller

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
@@ -40,23 +39,4 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// Encrypt password before save
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  try {
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-  } catch (error) {
-    if (process.env.NODE_ENV !== "production") {
-      console.error("Password hashing error:", error.message);
-    }
-    next(error);
-  }
-});
-
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
-
-const User = mongoose.model("User", userSchema);
-module.exports = User;
+module.exports = mongoose.model("User", userSchema);
