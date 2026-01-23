c45a8bf46f9c716b78c0aebe485925084714838e
Wahid ali <wahidali3150@gmail.com>
2026-01-23 20:01:37 +0500

    Header & Hero components are imported

--- Frontend/job-application-portal/src/pages/LandingPage/LandingPage.jsx
+++ Frontend/job-application-portal/src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <Hero />
+    </div>
+  );
 };
 
 export default LandingPage;
