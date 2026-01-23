003b760c02afc285335dc28e41d1c721b389deec
Wahid ali <wahidali3150@gmail.com>
2026-01-23 12:13:05 +0500

    All routes are created and requried their pages

--- Frontend/job-application-portal/src/App.jsx
+++ Frontend/job-application-portal/src/App.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import SignUp from "./pages/Auth/Signup";
+import Login from "./pages/Auth/Login";
+import JobSeekerDashboard from "./pages/Jobseeker/JobSeekerDashboard";
+import JobDetails from "./pages/Jobseeker/JobDetails";
+import SavedJobs from "./pages/Jobseeker/SavedJobs";
+import UserProfile from "./pages/Jobseeker/UserProfile";
+import EmployerDashboard from "./pages/Employer/EmployerDashboard";
+import JobPostingForm from "./pages/Employer/JobPostingForm";
+import ManageJobs from "./pages/Employer/ManageJobs";
+import ApplicationViewer from "./pages/Employer/ApplicationViewer";
+import EmployerProfilePage from "./pages/Employer/EmployerProfilePage";
+import ProtectedRoute from "./pages/routes/ProtectedRoute";
 
 const App = () => {
-  return <div className="text-2xl text-red-500">App</div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          // Public Routes
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/find-jobs" element={<JobSeekerDashboard />} />
+          <Route path="/job/:jobId" element={<JobDetails />} />
+          <Route path="/saved-jobs" element={<SavedJobs />} />
+          <Route path="/profile" element={<UserProfile />} />
+          // Protected Routes
+          <Route element={<ProtectedRoute requiredRole="employer" />}>
+            <Route path="/employer-dashboard" element={<EmployerDashboard />} />
+            <Route path="/post-job" element={<JobPostingForm />} />
+            <Route path="/manage-jobs" element={<ManageJobs />} />
+            <Route path="/application" element={<ApplicationViewer />} />
+            <Route path="/company-profile" element={<EmployerProfilePage />} />
+          </Route>
+          // Catch all Routes
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px",
+          },
+        }}
+      />
+    </div>
+  );
 };
 
 export default App;
