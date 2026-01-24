5eff10ac886a4005d30e7f7efa4d8936d1fee15f
Wahid ali <wahidali3150@gmail.com>
2026-01-24 13:38:35 +0500

    Api routes are imported and added protection on authenticated routes

--- Frontend/job-application-portal/src/App.jsx
+++ Frontend/job-application-portal/src/App.jsx
@@ -5,6 +5,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./context/AuthContext";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import SignUp from "./pages/Auth/Signup";
 import Login from "./pages/Auth/Login";
@@ -21,38 +22,46 @@ import ProtectedRoute from "./pages/routes/ProtectedRoute";
 
 const App = () => {
   return (
-    <div>
-      <Router>
-        <Routes>
-          // Public Routes
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/find-jobs" element={<JobSeekerDashboard />} />
-          <Route path="/job/:jobId" element={<JobDetails />} />
-          <Route path="/saved-jobs" element={<SavedJobs />} />
-          <Route path="/profile" element={<UserProfile />} />
-          // Protected Routes
-          <Route element={<ProtectedRoute requiredRole="employer" />}>
-            <Route path="/employer-dashboard" element={<EmployerDashboard />} />
-            <Route path="/post-job" element={<JobPostingForm />} />
-            <Route path="/manage-jobs" element={<ManageJobs />} />
-            <Route path="/application" element={<ApplicationViewer />} />
-            <Route path="/company-profile" element={<EmployerProfilePage />} />
-          </Route>
-          // Catch all Routes
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </Router>
-      <Toaster
-        toastOptions={{
-          className: "",
-          style: {
-            fontSize: "13px",
-          },
-        }}
-      />
-    </div>
+    <AuthProvider>
+      <div>
+        <Router>
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/find-jobs" element={<JobSeekerDashboard />} />
+            <Route path="/job/:jobId" element={<JobDetails />} />
+            <Route path="/saved-jobs" element={<SavedJobs />} />
+            <Route path="/profile" element={<UserProfile />} />
+            {/* Protected Routes */}
+            <Route element={<ProtectedRoute requiredRole="employer" />}>
+              <Route
+                path="/employer-dashboard"
+                element={<EmployerDashboard />}
+              />
+              <Route path="/post-job" element={<JobPostingForm />} />
+              <Route path="/manage-jobs" element={<ManageJobs />} />
+              <Route path="/application" element={<ApplicationViewer />} />
+              <Route
+                path="/company-profile"
+                element={<EmployerProfilePage />}
+              />
+            </Route>
+            {/* Catch all Routes */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </Router>
+        <Toaster
+          toastOptions={{
+            className: "",
+            style: {
+              fontSize: "13px",
+            },
+          }}
+        />
+      </div>
+    </AuthProvider>
   );
 };
 
