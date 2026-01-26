302440f1a5efabce2887d1de90a3ae84c7ea4790
Wahid ali <wahidali3150@gmail.com>
2026-01-26 15:38:41 +0500

    feat: update app routing and component structure

--- Frontend/job-application-portal/src/App.jsx
+++ Frontend/job-application-portal/src/App.jsx
@@ -13,11 +13,14 @@ import JobSeekerDashboard from "./pages/Jobseeker/JobSeekerDashboard";
 import JobDetails from "./pages/Jobseeker/JobDetails";
 import SavedJobs from "./pages/Jobseeker/SavedJobs";
 import UserProfile from "./pages/Jobseeker/UserProfile";
+import MyApplications from "./pages/Jobseeker/MyApplications";
 import EmployerDashboard from "./pages/Employer/EmployerDashboard";
 import JobPostingForm from "./pages/Employer/JobPostingForm";
 import ManageJobs from "./pages/Employer/ManageJobs";
 import ApplicationViewer from "./pages/Employer/ApplicationViewer";
 import EmployerProfilePage from "./pages/Employer/EmployerProfilePage";
+import EditProfileDetails from "./pages/Employer/EditProfileDetails";
+import EmployerJobDetails from "./pages/Employer/EmployerJobDetails";
 import ProtectedRoute from "./pages/routes/ProtectedRoute";
 
 const App = () => {
@@ -30,27 +33,32 @@ const App = () => {
             <Route path="/" element={<LandingPage />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
+            {/* Public job listing - redirects authenticated users to their dashboard */}
             <Route path="/find-jobs" element={<JobSeekerDashboard />} />
             <Route path="/job/:jobId" element={<JobDetails />} />
             {/* Protected Jobseeker Routes */}
             <Route element={<ProtectedRoute requiredRole="jobseeker" />}>
               <Route path="/jobseeker-dashboard" element={<JobSeekerDashboard />} />
               <Route path="/saved-jobs" element={<SavedJobs />} />
+              <Route path="/my-applications" element={<MyApplications />} />
               <Route path="/profile" element={<UserProfile />} />
             </Route>
-            {/* Protected Routes */}
+            {/* Protected Employer Routes */}
             <Route element={<ProtectedRoute requiredRole="employer" />}>
               <Route
                 path="/employer-dashboard"
                 element={<EmployerDashboard />}
               />
               <Route path="/post-job" element={<JobPostingForm />} />
+              <Route path="/edit-job/:jobId" element={<JobPostingForm />} />
+              <Route path="/employer/job/:jobId" element={<EmployerJobDetails />} />
               <Route path="/manage-jobs" element={<ManageJobs />} />
-              <Route path="/application" element={<ApplicationViewer />} />
+              <Route path="/applications" element={<ApplicationViewer />} />
               <Route
                 path="/company-profile"
                 element={<EmployerProfilePage />}
               />
+              <Route path="/edit-profile" element={<EditProfileDetails />} />
             </Route>
             {/* Catch all Routes */}
             <Route path="*" element={<Navigate to="/" replace />} />
